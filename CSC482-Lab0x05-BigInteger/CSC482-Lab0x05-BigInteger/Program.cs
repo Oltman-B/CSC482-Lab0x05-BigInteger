b0e0f01a4b059e9d8ea424bc08c3df6344bd5d5e
Baruch Oltman <42524526+Oltman-B@users.noreply.github.com>
2020-10-25 14:01:51 -0500

    Implements MyBigInteger using naieve string method to store the value. I want to try and implement this with something more efficient, but the math algorithms work as do the converted fibonacci functions. I need to edit the calculators for the benchmarker, those are out of date.

---
+++ CSC482-Lab0x05-BigInteger/CSC482-Lab0x05-BigInteger/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSC482_Lab0x05_BigInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sandbox = new FibSandbox();
+            if (sandbox.RunVerificationTests())
+            {
+                Console.WriteLine("Verification Tests Passed!");
+                sandbox.RunTimeTests();
+            }
+            else
+            {
+                Console.WriteLine("One or more verification tests failed.");
+            }
+        }
+    }
+}
