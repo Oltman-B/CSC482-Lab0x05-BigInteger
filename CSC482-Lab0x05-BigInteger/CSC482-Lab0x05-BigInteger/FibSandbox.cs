d2c353e914e72d45c0ead9b953e3b5579ecb812f
Baruch Oltman <42524526+Oltman-B@users.noreply.github.com>
2020-10-25 16:16:02 -0500

    Add missing files.

---
+++ CSC482-Lab0x05-BigInteger/CSC482-Lab0x05-BigInteger/FibSandbox.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSC482_Lab0x05_BigInteger
+{
+    class FibSandbox
+    {
+        public long FibRecur(int x)
+        {
+            if (x < 2) return x;
+
+            return FibRecur(x-1) + FibRecur(x-2);
+        }
+
+        public void FibRecurDoublingCalc(AlgStats algStats)
+        {
+
+            // Only calculate doubling ratios for even values of n and if n/2 exists in prev times table.
+            if (algStats.n % 2 == 0 && algStats.PrevTimesTable.TryGetValue(algStats.n / 2, out double halfTime))
+            {
+                algStats.ActualDoublingRatio = (algStats.TimeMicro / halfTime);
+
+                double goldenRatio = 1.61803398875; 
+                algStats.ExpectedDoublingRatio = Math.Pow(goldenRatio, algStats.n) / Math.Pow(goldenRatio, ((double)algStats.n / 2));
+            }
+            else
+            {
+                algStats.ActualDoublingRatio = -1;
+                algStats.ExpectedDoublingRatio = -1;
+            }
+
+        }
+
+        public long FibCache(int x)
+        {
+            var cache = new Dictionary<int, long>();
+            return FibCache(x, cache);
+        }
+
+        private long FibCache(int x, Dictionary<int, long> cache)
+        {
+            if (x < 2) return x;
+
+            // Use cache to look-up x and get result if already calculated
+            // If cache contains x, just return the stored fib result
+            if (cache.ContainsKey(x)) return cache[x];
+
+            // If cache does not contain x, store the fib result
+            cache[x] = FibCache(x - 1, cache) + FibCache(x - 2, cache);
+            // and return the result
+            return cache[x];
+        }
+
+        public void FibCacheDoublingCalc(AlgStats algStats)
+        {
+            // Only calculate doubling ratios for even values of n and if n/2 exists in prev times table.
+            if (algStats.n % 2 == 0 && algStats.PrevTimesTable.TryGetValue(algStats.n / 2, out double halfTime))
+            {
+                algStats.ActualDoublingRatio = (algStats.TimeMicro / halfTime);
+                algStats.ExpectedDoublingRatio = algStats.n / ((double)algStats.n / 2);
+            }
+            else
+            {
+                algStats.ActualDoublingRatio = -1;
+                algStats.ExpectedDoublingRatio = -1;
+            }
+        }
+
+        public MyBigInteger FibLoop(int x)
+        {
+            
+            if (x < 2) return new MyBigInteger(x.ToString());
+
+            MyBigInteger fib1 = new MyBigInteger("0");
+            MyBigInteger fib2 = new MyBigInteger("1");
+
+            // Iterate from 2->x and calculate each proceeding term of the fib
+            // sequence until we have calculated the final xth term
+            for (int i = 2; i <= x; i++)
+            {
+                MyBigInteger nextFib = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = nextFib;
+            }
+
+            // Return xth fibonacci.
+            return fib2;
+        }
+
+        public void FibLoopDoublingCalc(AlgStats algStats)
+        {
+            // Only calculate doubling ratios for even values of n and if n/2 exists in prev times table.
+            if (algStats.n % 2 == 0 && algStats.PrevTimesTable.TryGetValue(algStats.n / 2, out double halfTime))
+            {
+                algStats.ActualDoublingRatio = (algStats.TimeMicro / halfTime);
+                algStats.ExpectedDoublingRatio = algStats.n / ((double)algStats.n / 2);
+            }
+            else
+            {
+                algStats.ActualDoublingRatio = -1;
+                algStats.ExpectedDoublingRatio = -1;
+            }
+        }
+
+        public MyBigInteger FibMatrix(int x)
+        {
+            var multiplier = new MyBigInteger[2, 2] {{new MyBigInteger("1"), new MyBigInteger("1")}, {new MyBigInteger("1"), new MyBigInteger("0")}};
+            var result = CalculatePowerMatrix(multiplier, x);
+
+            // The way I implemented calculate power matrix results in the fibonacci value
+            // stored in the bottom right corner of matrix rather than top left.
+            return result[1, 1];
+        }
+
+        public void FibMatrixDoublingCalc(AlgStats algStats)
+        {
+            // Only calculate doubling ratios for even values of n and if n/2 exists in prev times table. n must be > 2 for log, otherwise div by 0.
+            if (algStats.n > 2 && algStats.n % 2 == 0 && algStats.PrevTimesTable.TryGetValue(algStats.n / 2, out double halfTime))
+            {
+                algStats.ActualDoublingRatio = (algStats.TimeMicro / halfTime);
+                algStats.ExpectedDoublingRatio = Math.Log2(algStats.n) / Math.Log2(((double)algStats.n / 2));
+            }
+            else
+            {
+                algStats.ActualDoublingRatio = -1;
+                algStats.ExpectedDoublingRatio = -1;
+            }
+        }
+
+        // Similar to the fast calculate power int
+        private MyBigInteger[,] CalculatePowerMatrix(MyBigInteger[,] m, long y)
+        {
+            //Calculates m^y
+            if (y == 0) return new MyBigInteger[2, 2]
+                {{new MyBigInteger("1"), new MyBigInteger("1")}, {new MyBigInteger("1"), new MyBigInteger("0")}};
+
+            var doublePower = m;
+            var result = new MyBigInteger[2, 2]
+                {{new MyBigInteger("1"), new MyBigInteger("1")}, {new MyBigInteger("1"), new MyBigInteger("0")}};
+
+            // while there are digits left to process
+            while (y > 0)
+            {
+                // if current low-order digit is 1
+                if ((y & 1) > 0)
+                {
+                    // multiply current matrix by the value of the doublePower matrix
+                    result = MultiplyMatrices(result, doublePower);
+                }
+
+                // move next digit to low order position
+                y >>= 1;
+
+                // just like with ints, the power can be doubled for each 'bit position' in the 
+                // y value. this will cut down on the number of matrix multiplications required.
+                //13 ->  b:     1            1           0 <- skip 1
+                //x^13 = M^8 * (1)  * M^4 * (1) * M^2 * (0) * M * (1)
+                doublePower = MultiplyMatrices(doublePower, doublePower);
+            }
+
+            return result;
+        }
+
+        private long CalculatePowerLong(long x, long y)
+        {
+            if (y == 0) return 1;
+
+            long doublePower = x;
+            long result = 1;
+            while (y > 0)
+            {
+                // if low-order bit is 1, do calculation
+                if ((y & 1) > 0)
+                {
+                    result *= (doublePower);
+                }
+
+                // shift to next highest bit to low order position
+                y >>= 1;
+
+                //13 ->  b:     1            1           0         1
+                //x^13 = x^8 * (1)  * x^4 * (1) * x^2 * (0) * x * (1)
+                doublePower *= doublePower;
+            }
+
+            return result;
+        }
+
+        private MyBigInteger[,] MultiplyMatrices(MyBigInteger[,] A, MyBigInteger[,] B)
+        {
+            var result = new MyBigInteger[2,2]
+                {{new MyBigInteger("0"), new MyBigInteger("0")}, {new MyBigInteger("0"), new MyBigInteger("0")}};
+            // iterate through A's rows
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                // iterate B's columns
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    // iterate B's rows
+                    for (int k = 0; k < B.GetLength(0); k++)
+                    {
+                        // sum product of each element in A's row and B's column
+                        result[i,j] += A[i,k] * B[k,j];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public void RunTimeTests()
+        {
+            var benchmarker = new AlgorithmBenchmarker();
+            //benchmarker.AddAlgorithmToBenchmark(FibRecur, FibRecurDoublingCalc);
+            //benchmarker.AddAlgorithmToBenchmark(FibCache, FibCacheDoublingCalc);
+            //benchmarker.AddAlgorithmToBenchmark(FibLoop, FibLoopDoublingCalc);
+            benchmarker.AddAlgorithmToBenchmark(FibMatrix, FibMatrixDoublingCalc);
+
+            benchmarker.RunTimeTests();
+        }
+
+        public bool RunVerificationTests()
+        {
+            const int tenthFibonacci = 55;
+
+            if (FibRecur(10) != tenthFibonacci) return false;
+            if (FibCache(10) != tenthFibonacci) return false;
+            //if (FibLoop(10) != tenthFibonacci) return false;
+
+            if (CalculatePowerLong(10, 5) != 100000) return false;
+            if (CalculatePowerLong(3, 22) != 31381059609) return false;
+
+            return true;
+        }
+    }
+}
---
+++ CSC482-Lab0x05-BigInteger/CSC482-Lab0x05-BigInteger/MyBigInteger.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CSC482_Lab0x05_BigInteger
+{
+    class MyBigInteger : IComparable<MyBigInteger>
+    {
+        private StringBuilder _value;
+        private bool _negative = false;
+
+        public MyBigInteger(string bigInteger)
+        {
+            _value = new StringBuilder(bigInteger);
+        }
+
+        public int GetLength()
+        {
+            return _value.Length;
+        }
+
+        private char GetDigitChar(int index)
+        {
+            return _value[index];
+        }
+
+        private void PadToWidth(int width)
+        {
+            int length = this.GetLength();
+            if (length < width)
+            {
+                for (int i = 0; i < width - length; i++)
+                {
+                    _value.Insert(0, "0");
+                }
+            }
+        }
+
+        public MyBigInteger Plus(MyBigInteger valB)
+        {
+            int carry = 0;
+            StringBuilder result = new StringBuilder();
+
+            // Pad shorter number
+            valB.PadToWidth(Math.Max(this.GetLength(), valB.GetLength()));
+            this.PadToWidth(Math.Max(this.GetLength(), valB.GetLength()));
+
+            for (int i = GetLength() - 1; i >= 0; i--)
+            {
+                int digA = AsciiToDigit(this.GetDigitChar(i));
+                int digB = AsciiToDigit(valB.GetDigitChar(i));
+
+                int sum = digA + digB + carry;
+                int digC = sum;
+                if (sum >= 10)
+                {
+                    carry = 1;
+                    digC = sum - 10;
+                }
+                else
+                {
+                    carry = 0;
+                }
+
+                result.Insert(0, DigitToAscii(digC));
+            }
+
+            if (carry != 0) result.Insert(0, DigitToAscii(carry));
+            return new MyBigInteger(result.ToString());
+        }
+        public MyBigInteger Times(MyBigInteger valB)
+        {
+            // Got some guidance from (https://www.geeksforgeeks.org/multiply-large-numbers-represented-as-strings/)
+            // I want to spend some time coming up with a new solution before attempting the fast multiplication algorithm
+
+            // Handle exceptional cases that can avoid entering the loops.
+            if(this.GetLength() == 0 || valB.GetLength() == 0) return new MyBigInteger("0");
+            if (this.GetLength() == 1 && this.GetDigitChar(0) == '1') return new MyBigInteger(valB.ToString());
+            if (valB.GetLength() == 1 && valB.GetDigitChar(0) == '1') return new MyBigInteger(this.ToString());
+            if (this.GetLength() == 1 && this.GetDigitChar(0) == '0') return new MyBigInteger("0");
+            if (valB.GetLength() == 1 && valB.GetDigitChar(0) == '0') return new MyBigInteger("0");
+
+            StringBuilder result = new StringBuilder(2 * this.GetLength());
+
+            // Pad shorter number 
+            valB.PadToWidth(Math.Max(this.GetLength(), valB.GetLength()));
+            this.PadToWidth(Math.Max(this.GetLength(), valB.GetLength()));
+
+            // max width of result is 2x size of longest value, values are padded so just 2x this.Length
+            int[] multiplicationSum = new int[this.GetLength() * 2];
+
+            int thisIndx = 0;
+            for (int i = GetLength() - 1; i >= 0; i--)
+            {
+                int carry = 0;
+                int digA = AsciiToDigit(this.GetDigitChar(i));
+
+                int valBindx = 0;
+                for (int j = GetLength() - 1; j >= 0; j--)
+                {
+                    int digB = AsciiToDigit(valB.GetDigitChar(j));
+
+                    int sum = digA * digB + multiplicationSum[thisIndx + valBindx] + carry;
+
+                    carry = sum / 10;
+
+                    multiplicationSum[thisIndx + valBindx] = sum % 10;
+
+                    // Multiply each digit in b by the current digit of this (a)
+                    valBindx++;
+                }
+
+                if (carry > 0)
+                {
+                    multiplicationSum[thisIndx + valBindx] += carry;
+                }
+
+                thisIndx++;
+            }
+            // Count leading 0 in unused section of result
+            int zeroCount = 0;
+            while (multiplicationSum[multiplicationSum.Length - zeroCount - 1] == 0)
+            {
+                zeroCount++;
+            }
+
+            for (int i = multiplicationSum.Length - zeroCount - 1; i >= 0; i--)
+            {
+                result.Append(DigitToAscii(multiplicationSum[i]));
+            }
+
+            return new MyBigInteger(result.ToString());
+        }
+
+        public int CompareTo(MyBigInteger other)
+        {
+            if (this._value.Equals(other._value) && this._negative == other._negative) return 0;
+
+            // Need to think about implementing other comparisons if we ever need to sort etc...
+            else return -1;
+        }
+
+        public override string ToString()
+        {
+            return this._value.ToString();
+        }
+
+        public static MyBigInteger operator +(MyBigInteger a, MyBigInteger b)
+        {
+            return a.Plus(b);
+        }
+
+        public static MyBigInteger operator *(MyBigInteger a, MyBigInteger b)
+        {
+            return a.Times(b);
+        }
+
+        public static bool operator ==(MyBigInteger a, MyBigInteger b)
+        {
+            return a.CompareTo(b) == 0 ? true : false;
+        }
+
+        public static bool operator !=(MyBigInteger a, MyBigInteger b)
+        {
+            return a.CompareTo(b) != 0 ? true : false;
+        }
+
+        private int AsciiToDigit(char c)
+        {
+            return c - '0';
+        }
+
+        private char DigitToAscii(int d)
+        {
+            return (char)(d + '0');
+        }
+
+        private List<int> B10StringToB32BigInt(string value)
+        {
+            int b10Multiplier = 1;
+            int currentB32Digit = 0;
+            List<int> b32Value = new List<int>();
+
+            for (int i = value.Length - 1; i >= 0; i--, b10Multiplier *= 10)
+            {
+                
+            }
+
+            return b32Value;
+        }
+    }
+}
